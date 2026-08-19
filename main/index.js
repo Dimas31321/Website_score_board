089fdd02dc4d12cb6ddb69768f0ff8b4071fd751
Dmitry <dl030507@gmail.com>
2026-08-19 20:08:02 +0300

    Added basic funcions and logic to JS file

---
+++ main/index.js
@@ -0,0 +1,14 @@
+let cntLeft = 0
+let cntRight = 0
+
+function incrementBy1(count){
+    count += 1
+}
+
+function incrementBy2(count){
+    count += 2
+}
+
+function incrementBy3(count){
+    count += 3
+}
